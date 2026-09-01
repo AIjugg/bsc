44057f7d1f57f339a86fb61653b360aec27287c8
BandariThomas <30991820+AIjugg@users.noreply.github.com>
2026-09-01 22:18:12 +0800

    Update rlp_test.go

--- tests/rlp_test.go
+++ tests/rlp_test.go
@@ -13,7 +13,9 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
+//  /\_/\ 
+// ( ·.· )
+//  > ^ < 
 package tests
 
 import (
